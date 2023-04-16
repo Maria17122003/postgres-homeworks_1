6a2ee685319782b438e1bbe2d00efbe1aecd3c1f
Maria <mararyabina62@gmail.com>
2023-04-16 23:37:46 +1000

    some changes

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+CREATE DATABASE north
+
+CREATE TABLE customers_data
+(
+	customer_id	int PRIMARY KEY,
+	company_name varchar(100),
+	contact_name varchar(100)
+);
+
+CREATE TABLE employees_data
+(
+	first_name	varchar(50) PRIMARY KEY,
+	last_name varchar(50),
+	title varchar(100),
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE orders_data
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(50) REFERENCES customers_data(customer_id),
+	employee_id varchar(100),
+	order_date date,
+	ship_city text
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,45 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+import os
+
+customers_data = os.path.join('north_data', 'customers_data.csv')
+employees_data = os.path.join('north_data', 'employees_data.csv')
+orders_data = os.path.join('north_data', 'orders_data.csv')
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="17122003M"
+)
+
+cur = conn.cursor()
+with open(customers_data) as file:
+    file_csv = csv.DictReader(file)
+    for item in file_csv:
+        cur.execute(
+                    "INSERT INTO customers_data VALUES (%s, %s, %s)",
+                    (item['customer_id'], item['company_name'], item['contact_name'])
+                    )
+
+with open(employees_data) as file:
+    file_csv = csv.DictReader(file)
+    for item in file_csv:
+        cur.execute(
+                    "INSERT INTO employees_data VALUES (%s, %s, %s, %s, %s)",
+                    (item['first_name'], item['last_name'], item['title'], item['birth_date'],
+                     item['notes'])
+                    )
+
+with open(orders_data) as file:
+    file_csv = csv.DictReader(file)
+    for item in file_csv:
+        cur.execute(
+                    "INSERT INTO orders_data VALUES (%s, %s, %s, %s, %s)",
+                    (item['order_id'], item['customer_id'], item['employee_id'], item['order_date'],
+                     item['ship_city'])
+                    )
+
+conn.commit()
+cur.close()
+conn.close()
