ca6af481b58b6d0b32ddb08f89f595f405c5cd41
Maria17122003 <123393865+Maria17122003@users.noreply.github.com>
2023-04-24 19:14:12 +1000

    Update alter_student.sql

--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE student
 (
-	student_id serial PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	student_id serial PRIMARY KEY,
 	first_name varchar(50),
 	last_name varchar(50),
 	birthday date,
